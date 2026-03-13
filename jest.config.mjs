f86c8511db05371ae1691ab0767e440736fe9c50
Souhail Miliani <souhailmiliani13@gmail.com>
2026-03-13 02:11:34 +0100

    Add Jest ESM configuration and update test scripts

---
+++ jest.config.mjs
@@ -0,0 +1,9 @@
+export default {
+  testEnvironment: "node",
+  testMatch: ["**/__tests__/**/*.test.js"],
+  collectCoverageFrom: [
+    "src/**/*.js",
+    "!src/server.js",
+    "!src/config/**",
+  ],
+};
